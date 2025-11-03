814e407ada3e757b64cb1d2dc7accfe9a943569c
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-11-03 18:03:36 +0000

    Update vscode scripts with necessary packages

--- docker/apps_install_scripts/vscode.sh
+++ docker/apps_install_scripts/vscode.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+echo "Installing system dependencies..."
+sudo apt-get update
+sudo apt-get install -y \
+    build-essential \
+    libx11-dev \
+    libxkbfile-dev \
+    libsecret-1-dev \
+    pkg-config
+
 cd /workspace/PC-Canary/apps/vscode/
 
+echo "Cleaning the npm cache"
 npm cache clean --force
-yarn run compile
+
+echo "Installing required packages"
+npm install
+
+# yarn run compile
+
+echo "Compiling VSCode"
+npm run compile
 
 # Add symbolic link to code-cli
 sudo ln -sf /workspace/PC-Canary/apps/vscode/scripts/code-cli.sh /usr/local/bin/code
